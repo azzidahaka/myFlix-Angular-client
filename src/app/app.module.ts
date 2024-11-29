e6fe647ca756c8e4495bd9abead0224582034317
Azzidahaka <Azzidahaka@gmail.com>
2024-11-28 18:06:46 -0700

    created moviecard component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,9 +15,10 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { FormsModule } from '@angular/forms';
 import { UserLoginComponent } from './user-login/user-login.component';
+import { MovieCardComponent } from './movie-card/movie-card.component';
 
 @NgModule({
-  declarations: [AppComponent, UserRegistrationFormComponent, UserLoginComponent],
+  declarations: [AppComponent, UserRegistrationFormComponent, UserLoginComponent, MovieCardComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
